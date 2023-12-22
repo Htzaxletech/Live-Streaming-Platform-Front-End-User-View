b605198622bd60bce0ab274a319c5cfd7162b653
Hsu <myathsu703@gmail.com>
2023-12-22 11:18:13 +0630

    sidebar

--- src/components/shared/Layout.tsx
+++ src/components/shared/Layout.tsx
@@ -1,12 +1,12 @@
 import { PropsWithChildren } from "react"
-
 import Navbar from "./Navbar"
 import Footer from "./Footer"
-
+import {Sidebar} from "../ui/Sidebar" 
 const Layout = ({ children }: PropsWithChildren) => {
   return (
     <div className="text-sm">
       <Navbar />
+      <Sidebar/>
       <main className="p-5">{children}</main>
       <Footer />
     </div>
